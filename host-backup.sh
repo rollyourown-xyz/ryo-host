d08f88746495c9898fe630065db8de09f7b4815a
xyzroller <xyzroller@rollyourown.xyz>
2022-02-03 20:12:31 +0100

    Modify scripts to call module and project scripts

--- host-backup.sh
+++ host-backup.sh
@@ -47,94 +47,141 @@ then
 fi
 
 
-# Script directory
+# Variables: Script directory, list of projects, list of modules
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECTS="$( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml )"
+MODULES="$( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml )"
 
 
-# Info - Starting
-#################
-
+# Ask whether to backup all projects and modules
 echo ""
-echo "Starting backup for "$hostname""
-
-
-# Stop project containers
-##########################
-
+echo "Back up all projects and modules on "$hostname"?"
+echo "Projects deployed on the host are: "$PROJECTS""
+echo "Modules deployed on the host are: "$MODULES""
 echo ""
-echo "Stopping project containers on "$hostname""
+echo "To back up all projects and modules on the host "$hostname", then answer 'y' (the default)."
+echo "If you choose 'n', you will be able to select whether to back up each project individually (with its modules)."
+echo ""
+echo -n "Back up all projects and modules on "$hostname"? "
+read -e -p "[y/n]: " BACKUP_ALL
+BACKUP_ALL="${BACKUP_ALL:-"y"}"
+BACKUP_ALL="${BACKUP_ALL,,}"
 
-for project in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml ); do
-  echo ""
-  echo "...stopping project containers for "$project""
-  /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/stop-project-containers.sh -n "$hostname"
+# Check input
+while [ ! "$BACKUP_ALL" == "y" ] && [ ! "$BACKUP_ALL" == "n" ]
+do
+  echo "Invalid option "${BACKUP_ALL}". Please try again."
+  echo -n "Back up all projects and modules on "$hostname"? "
+  read -e -p "[y/n]: " BACKUP_ALL
+  BACKUP_ALL="${BACKUP_ALL:-"y"}"
+  BACKUP_ALL="${BACKUP_ALL,,}"
 done
 
-
-# Stop module containers
-########################
-
-echo ""
-echo "Stopping module containers on "$hostname""
-
-for module in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml ); do
+if [ "$BACKUP_ALL" == "y" ]; then
   echo ""
-  echo "...stopping module containers for "$module""
-  /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/stop-module-containers.sh -n "$hostname"
-done
+  echo "Backing up all projects and modules."
 
-
-# Back up project container persistent storage
-##############################################
-
-echo ""
-echo "Backing up project container persistent storage on "$hostname" with stamp "$stamp""
-
-for project in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml ); do
   echo ""
-  echo "Backing up "$project" container persistent storage on "$hostname" with stamp "$stamp""
-  ansible-playbook -i "$SCRIPT_DIR"/configuration/inventory_"$hostname" "$SCRIPT_DIR"/backup-restore/backup-project.yml --extra-vars "id="$project" host_id="$hostname" backup_stamp="$stamp""
-done
+  echo "Stopping project containers on "$hostname""
+  for project in $PROJECTS; do
+    /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/stop-project-containers.sh -n "$hostname"
+  done
 
-
-# Back up module container persistent storage
-#############################################
-
-echo ""
-echo "Backing up module container persistent storage on "$hostname" with stamp "$stamp""
-
-for module in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml ); do
   echo ""
-  echo "Backing up "$module" container persistent storage on "$hostname" with stamp "$stamp""
-  ansible-playbook -i "$SCRIPT_DIR"/configuration/inventory_"$hostname" "$SCRIPT_DIR"/backup-restore/backup-module.yml --extra-vars "id="$module" host_id="$hostname" backup_stamp="$stamp""
-done
+  echo "Stopping module containers on "$hostname""
+  for module in $MODULES; do
+    /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/stop-module-containers.sh -n "$hostname"
+  done
 
-
-# Start module containers
-#########################
-
-echo ""
-echo "Starting module containers on "$hostname""
-
-for module in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml ); do
   echo ""
-  echo "...starting module containers for "$module""
-  /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/start-module-containers.sh -n "$hostname"
-done
+  echo "Backing up all project container persistent storage on "$hostname" with stamp "$stamp""
+  for project in $PROJECTS; do
+    /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-module/backup-project.sh -n "$hostname" -s "$stamp"
+  done
 
-
-# Start project containers
-##########################
-
-echo ""
-echo "Starting project containers on "$hostname""
-
-for project in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml ); do
   echo ""
-  echo "...starting project containers for "$project""
-  /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/start-project-containers.sh -n "$hostname"
-done
+  echo "Backing up all module container persistent storage on "$hostname" with stamp "$stamp""
+  for module in $MODULES; do
+    /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/backup-module.sh -n "$hostname" -s "$stamp"
+  done
 
+  echo ""
+  echo "Starting module containers on "$hostname""
+  for module in $MODULES; do
+    /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/start-module-containers.sh -n "$hostname"
+  done
+  
+  echo ""
+  echo "Starting project containers on "$hostname""
+  for project in $PROJECTS; do
+    /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/start-project-containers.sh -n "$hostname"
+  done
+
+else
+  echo ""
+  echo "Checking for each project."
+
+  for project in $PROJECTS; do
+
+    # Get user input for whether to do individual project backup (default no)
+    echo ""
+    echo "Checking whether to back up "$project" project."
+    echo "Default is 'n'."
+    echo -n "Back up "$project" project? "
+    read -e -p "[y/n]: " BACKUP_PROJECT
+    BACKUP_PROJECT="${BACKUP_PROJECT:-"n"}"
+    BACKUP_PROJECT="${BACKUP_PROJECT,,}"
+    
+    # Check input
+    while [ ! "$BACKUP_PROJECT" == "y" ] && [ ! "$BACKUP_PROJECT" == "n" ]
+    do
+      echo "Invalid option "${BACKUP_PROJECT}". Please try again."
+      echo -n "Back up "$project" project (default is 'n')? "
+      read -e -p "[y/n]: " BACKUP_PROJECT
+      BACKUP_PROJECT="${BACKUP_PROJECT:-"n"}"
+      BACKUP_PROJECT="${BACKUP_PROJECT,,}"
+    done
+
+    if [ "$BACKUP_PROJECT" == "y" ]; then
+      /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/backup-project.sh -n "$hostname" -s "$stamp"
+    else
+      echo ""
+      echo "Skipping "$project" project backup."
+    fi
+  done
+  
+  echo ""
+  echo "Checking for each module."
+  
+  for module in $MODULES; do
+
+    # Get user input for whether to do individual module backup (default no)
+    echo ""
+    echo "Checking whether to back up "$module" module."
+    echo "Default is 'n'."
+    echo -n "Back up "$module" module? "
+    read -e -p "[y/n]: " BACKUP_MODULE
+    BACKUP_MODULE="${BACKUP_MODULE:-"n"}"
+    BACKUP_MODULE="${BACKUP_MODULE,,}"
+    
+    # Check input
+    while [ ! "$BACKUP_MODULE" == "y" ] && [ ! "$BACKUP_MODULE" == "n" ]
+    do
+      echo "Invalid option "${BACKUP_MODULE}". Please try again."
+      echo -n "Back up "$module" module (default is 'n')? "
+      read -e -p "[y/n]: " BACKUP_MODULE
+      BACKUP_MODULE="${BACKUP_MODULE:-"n"}"
+      BACKUP_MODULE="${BACKUP_MODULE,,}"
+    done
+
+    if [ "$BACKUP_MODULE" == "y" ]; then
+      /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/backup-module.sh -n "$hostname" -s "$stamp"
+    else
+      echo ""
+      echo "Skipping "$module" module backup."
+    fi
+  done
+fi
 
 # Info - Completed
 ##################
--- host-restore.sh
+++ host-restore.sh
@@ -5,9 +5,9 @@
 # and should only be used to restore to a newly set up host server.
 #
 # ATTENTION!!!
-# Before restoring, the process will **delete** the state of the host on the control machine so that the current state
-# of the deployment will be overwritten. This will lead to a working host server no longer being manageable from the
-# control node.
+# Before restoring ALL projects, the process will **delete** the state of the projects and/or modules on the control machine 
+# so that the current state of the deployment will be overwritten. This will lead to a working host server no longer
+# being manageable from the control node.
 # THIS SHOULD NORMALLY ONLY BE DONE IN CASE A RESTORE IS NEEDED FOR DISASTER RECOVERY - e.g. AFTER A SYSTEM FAILURE
 
 
@@ -19,10 +19,9 @@ helpMessage()
   echo "host-restore.sh: Restore a previous backup of the persistent storage from the control node to the host"
   echo ""
   echo "Help: host-restore.sh"
-  echo "Usage: ./host-restore.sh -n hostname"
+  echo "Usage: ./host-restore.sh -n hostname -s stamp"
   echo "Flags:"
   echo -e "-n hostname \t\t(Mandatory) Name of the host to which to restore the project container persistent storage"
-  echo -e "-v version \t\t(Mandatory) Version stamp for the new images to deploy, e.g. 20210101-1"
   echo -e "-s stamp \t\t(Mandatory) A stamp (e.g. date, time, name) to identify the backup to restore to the host server"  
   echo -e "-h \t\t\tPrint this help message"
   echo ""
@@ -40,21 +39,26 @@ errorMessage()
 # Command-line input handling
 #############################
 
-while getopts n:v:s:h flag
+while getopts n:s:h flag
 do
   case "${flag}" in
     n) hostname=${OPTARG};;
-    v) version=${OPTARG};;
     s) stamp=${OPTARG};;
     h) helpMessage ;;
     ?) errorMessage ;;
   esac
 done
 
-if [ -z "$hostname" ] || [ -z "$version" ] || [ -z "$stamp" ]
+if [ -z "$hostname" ] || [ -z "$stamp" ]
 then
   errorMessage
 fi
+
+
+# Variables: Script directory, list of projects, list of modules
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECTS="$( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml )"
+MODULES="$( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml )"
 
 
 # Warn the user and get confirmation
@@ -63,216 +67,247 @@ fi
 echo " "
 echo "!!! CAUTION! "
 echo "!!! "
-echo "!!! A restore should normally only be carried out on a newly-configured host and "
-echo "!!! will redeploy all project and module components on the host server, "
+echo "!!! A restore of **ALL** projects and module should normally only be carried out on a "
+echo "!!! newly-configured host and will redeploy **ALL** project and module components on the host server, "
 echo "!!! with a previous state of the projects and modules from a backup "
 echo "!!! "
 echo "!!! This is usually only needed to restore after a system failure or to move "
 echo "!!! projects and modules to a new host server. "
 echo " "
-echo "!!! A restore should not be carried out for a host server with functioning "
-echo "!!! projects! "
 echo " "
-echo "Do you want to restore a backup on "$hostname"? (y/n)"
+echo "!!! A restore of **ALL** projects and modules should not be carried out for a host server with functioning "
+echo "!!! projects! "
+echo ""
+echo "Do you want to restore **ALL** projects and modules from a backup on "$hostname"? (y/n)"
 echo "Default is 'n'."
-echo -n "Restore backup? "
-read -e -p "[y/n]:" RESTORE_BACKUP
-RESTORE_BACKUP="${RESTORE_BACKUP:-"n"}"
-RESTORE_BACKUP="${RESTORE_BACKUP,,}"
+echo ""
+echo -n "Restore backup for **ALL** projects and modules on "$hostname"? "
+read -e -p "[y/n]: " RESTORE_ALL
+RESTORE_ALL="${RESTORE_ALL:-"n"}"
+RESTORE_ALL="${RESTORE_ALL,,}"
 
-if [ ! "$RESTORE_BACKUP" == "y" ] && [ ! "$RESTORE_BACKUP" == "n" ]; then
-  echo "Invalid option "${RESTORE_BACKUP}". Exiting"
-  exit 1
+# Check input
+while [ ! "$RESTORE_ALL" == "y" ] && [ ! "$RESTORE_ALL" == "n" ]
+do
+  echo "Invalid option "${RESTORE_ALL}". Please try again."
+  echo -n "Restore backup for **ALL** projects and modules on "$hostname"? "
+  read -e -p "[y/n]: " RESTORE_ALL
+  RESTORE_ALL="${RESTORE_ALL:-"n"}"
+  RESTORE_ALL="${RESTORE_ALL,,}"
+done
 
-elif [ "$RESTORE_BACKUP" == "n" ]; then
-  echo "Exiting"
-  exit 1
-
-else
+if [ "$RESTORE_ALL" == "y" ]; do
   echo ""
   echo "!!! ARE YOU SURE? "
   echo "!!! "
   echo "!!! If your system is currently working, you will lose "
   echo "!!! the current state and may not be able to recover it! "
   echo "!!! "
-  echo "!!! If you proceed, you will restore projects and modules "
+  echo "!!! If you proceed, you will restore **ALL** projects and modules "
   echo "!!! on "$hostname" from backups stamped with "$stamp" "
   echo "!!! "
   echo "!!! Please confirm by typing 'yes' for the next question "
   echo "!!! Default is 'no'."
   echo ""
-  echo -n "Are you sure that you want to restore a backup? "
-  read -e -p "[yes/no]:" RESTORE_BACKUP_SURE
-  RESTORE_BACKUP_SURE="${RESTORE_BACKUP_SURE:-"no"}"
-  RESTORE_BACKUP_SURE="${RESTORE_BACKUP_SURE,,}"
-  
-  if [ ! "$RESTORE_BACKUP_SURE" == "yes" ] && [ ! "$RESTORE_BACKUP_SURE" == "no" ]; then
-    echo "Invalid option "${RESTORE_BACKUP_SURE}". Exiting"
-    exit 1
+  echo -n "Are you sure that you want to restore a backup for ALL projects and modules on "$hostname"? "
+  read -e -p "[yes/no]: " RESTORE_ALL_SURE
+  RESTORE_ALL_SURE="${RESTORE_ALL_SURE:-"no"}"
+  RESTORE_ALL_SURE="${RESTORE_ALL_SURE,,}"
 
-  elif [ "$RESTORE_BACKUP_SURE" == "no" ]; then
-    echo "Exiting"
-    exit 1
-  
-  else
+  # Check input
+  while [ ! "$RESTORE_ALL_SURE" == "y" ] && [ ! "$RESTORE_ALL_SURE" == "n" ]
+  do
+    echo "Invalid option "${RESTORE_ALL_SURE}". Please try again."
+    echo -n "Are you sure that you want to restore a backup for ALL projects and modules on "$hostname"? "
+    read -e -p "[y/n]: " RESTORE_ALL_SURE
+    RESTORE_ALL_SURE="${RESTORE_ALL_SURE:-"no"}"
+    RESTORE_ALL_SURE="${RESTORE_ALL_SURE,,}"
+  done
+
+  if [ "$RESTORE_ALL_SURE" == "yes" ]; do
     echo ""
     echo "!!! Please confirm the stamp of the backup to restore "
     echo "!!! " 
-    echo "!!! You will be restoring from backups with stamp: "$stamp" "
+    echo "!!! You will be restoring ALL projects and modules on "$hostname" from backups with stamp: "$stamp" "
     echo "!!! "
     echo "!!! Please confirm by typing 'yes' for the next question "
     echo "!!! Default is 'no'."
     echo ""
     echo -n "Are you sure that the backup stamp is correct? "
-    read -e -p "[yes/no]:" RESTORE_BACKUP_STAMP_SURE
-    RESTORE_BACKUP_STAMP_SURE="${RESTORE_BACKUP_STAMP_SURE:-"no"}"
-    RESTORE_BACKUP_STAMP_SURE="${RESTORE_BACKUP_STAMP_SURE,,}"
-    
-    if [ ! "$RESTORE_BACKUP_STAMP_SURE" == "yes" ] && [ ! "$RESTORE_BACKUP_STAMP_SURE" == "no" ]; then
-      echo "Invalid option "${RESTORE_BACKUP_STAMP_SURE}". Exiting"
-      exit 1
+    read -e -p "[yes/no]: " RESTORE_ALL_STAMP_SURE
+    RESTORE_ALL_STAMP_SURE="${RESTORE_ALL_STAMP_SURE:-"no"}"
+    RESTORE_ALL_STAMP_SURE="${RESTORE_ALL_STAMP_SURE,,}"
 
-    elif [ "$RESTORE_BACKUP_STAMP_SURE" == "no" ]; then
-      echo "Exiting"
-      exit 1
+    # Check input
+    while [ ! "$RESTORE_ALL_STAMP_SURE" == "y" ] && [ ! "$RESTORE_ALL_STAMP_SURE" == "n" ]
+    do
+      echo "Invalid option "${RESTORE_ALL_STAMP_SURE}". Please try again."
+      echo -n "Are you sure that you want to restore a backup for ALL projects and modules on "$hostname"? "
+      read -e -p "[y/n]: " RESTORE_ALL_STAMP_SURE
+      RESTORE_ALL_STAMP_SURE="${RESTORE_ALL_STAMP_SURE:-"no"}"
+      RESTORE_ALL_STAMP_SURE="${RESTORE_ALL_STAMP_SURE,,}"
+    done
 
+    if [ "$RESTORE_ALL_STAMP_SURE" == "yes" ]; do
+
+      echo ""
+      echo "Starting restore of ALL projects and modules on "$hostname""
+
+      echo ""
+      echo -n "Please enter a new version stamp for the new images to deploy, e.g. 20210101-1: "
+      read -e -p "" VERSION_STAMP
+
+      # Delete terraform state for all projects on "$hostname" on the host
+      for project in $PROJECTS; do
+        echo ""
+        echo "Deleting terraform state for project "$project" on "$hostname""
+        /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/delete-terraform-state.sh -n "$hostname"
+      done
+
+      # Delete terraform state for all modules on the host
+      for module in $MODULES; do
+        echo ""
+        echo "Deleting terraform state for module "$module" on "$hostname""
+        /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/delete-terraform-state.sh -n "$hostname"
+      done
+
+      # Deploy all modules listed in deployed-modules file (using -r flag)
+      for module in $MODULES; do
+        echo ""
+        echo "Deploying module "$module" on "$hostname""
+        /bin/bash "$SCRIPT_DIR"/../"$module"/deploy.sh -n "$hostname" -v "$VERSION_STAMP" -r
+      done
+
+      # Deploy all projects listed in deployed-projects file (using -r and -s flags)
+      for project in $PROJECTS; do
+        echo ""
+        echo "Deploying project "$project" on "$hostname""
+        /bin/bash "$SCRIPT_DIR"/../"$project"/deploy.sh -n "$hostname" -v "$VERSION_STAMP" -r -s
+      done
+
+      # Stop project containers
+      for project in $PROJECTS; do
+        echo ""
+        echo "Stopping project containers for "$project" on "$hostname""
+        /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/stop-project-containers.sh -n "$hostname"
+      done
+
+      # Stop module containers
+      for module in $MODULES; do
+        echo ""
+        echo "Stopping module containers for "$module" on "$hostname""
+        /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/stop-module-containers.sh -n "$hostname"
+      done
+
+      # Restore module container persistent storage
+      for module in $MODULES; do
+        echo ""
+        echo "Restoring "$module" container persistent storage on "$hostname" with stamp "$stamp"" 
+        /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/restore-module.sh -n "$hostname" -s "$stamp"
+      done
+
+      # Restore project container persistent storage
+      for project in $PROJECTS; do
+        echo ""
+        echo "Restoring "$project" container persistent storage on "$hostname" with stamp "$stamp""
+        /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-module/restore-project.sh -n "$hostname" -s "$stamp"
+      done
+
+      # Start module containers
+      for module in $MODULES; do
+        echo ""
+        echo "Starting module containers for "$module" on "$hostname""
+        /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/start-module-containers.sh -n "$hostname"
+      done
+
+      # Start project containers
+      for project in $PROJECTS; do
+        echo ""
+        echo "Starting project containers for "$project" on "$hostname""
+        /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/start-project-containers.sh -n "$hostname"
+      done
+
+    else
+      echo ""
+      echo "Skipping restore of ALL projects and modules on "$hostname""
     fi
+
+  else
+    echo ""
+    echo "Skipping restore of ALL projects and modules on "$hostname""
   fi
+
+else
+  echo ""
+  echo "Checking whether to restore each project individually."
+
+  for project in $PROJECTS; do
+
+    # Get user input for whether to do individual project restore (default no)
+    echo ""
+    echo "Checking whether to restore "$project" project."
+    echo "Default is 'n'."
+    echo -n "Restore "$project" project on "$hostname"? "
+    read -e -p "[y/n]: " RESTORE_PROJECT
+    RESTORE_PROJECT="${RESTORE_PROJECT:-"n"}"
+    RESTORE_PROJECT="${RESTORE_PROJECT,,}"
+    
+    # Check input
+    while [ ! "$RESTORE_PROJECT" == "y" ] && [ ! "$RESTORE_PROJECT" == "n" ]
+    do
+      echo "Invalid option "${RESTORE_PROJECT}". Please try again."
+      echo -n "Restore "$project" project on "$hostname"? "
+      read -e -p "[y/n]: " RESTORE_PROJECT
+      RESTORE_PROJECT="${RESTORE_PROJECT:-"n"}"
+      RESTORE_PROJECT="${RESTORE_PROJECT,,}"
+    done
+
+    if [ "$RESTORE_PROJECT" == "y" ]; then
+
+      # Check the project stamp
+      echo ""
+      echo "You will be restoring the project "$project" from a backup with stamp "$stamp"."
+      echo "Please check the stamp "$stamp" carefully!"
+      echo -n "Is the stamp "$stamp" correct for the project "$project"? (default is 'y') "
+      read -e -p "[y/n]: " STAMP_CORRECT
+      STAMP_CORRECT="${STAMP_CORRECT:-"y"}"
+      STAMP_CORRECT="${STAMP_CORRECT,,}"
+
+      # Check input
+      while [ ! "$STAMP_CORRECT" == "y" ] && [ ! "$STAMP_CORRECT" == "n" ]
+      do
+        echo "Invalid option "${STAMP_CORRECT}". Please try again."
+        echo -n "Is the stamp "$stamp" correct for the project "$project"? (default is 'y') "
+        read -e -p "[y/n]: " STAMP_CORRECT
+        STAMP_CORRECT="${STAMP_CORRECT:-"y"}"
+        STAMP_CORRECT="${STAMP_CORRECT,,}"
+      done
+
+      if [ "$STAMP_CORRECT" == "y" ]; then
+        /bin/bash "$SCRIPT_DIR"/../"$project"/restore.sh -n "$hostname" -s "$stamp"
+
+      else
+        # Request different backup stamp
+        echo ""
+        echo "Please enter the stamp for the backup of project "$project" to restore."
+        echo "Please double-check you are entering the correct stamp."
+        echo ""
+        echo -n "What is the stamp for the backup of project "$project" to restore? "
+        read -e -p "" DIFFERENT_STAMP
+        echo ""
+        echo "Restoring the project "$project" to "$hostname" from a backup with stamp "$DIFFERENT_STAMP""
+        /bin/bash "$SCRIPT_DIR"/../"$project"/restore.sh -n "$hostname" -s "$DIFFERENT_STAMP"
+      fi
+
+    else
+      echo ""
+      echo "Skipping "$project" project restore."
+    fi
+  done
 fi
-
-
-# Script directory
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-
-# Info - Starting
-#################
-
-echo ""
-echo "Starting restore on "$hostname""
-
-
-# Delete terraform state for all projects on the host
-#####################################################
-
-for project in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml ); do
-  echo ""
-  echo "Deleting terraform state for project "$project" on "$hostname""
-  /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/delete-terraform-state.sh -n "$hostname"
-done
-
-
-# Delete terraform state for all modules on the host
-####################################################
-
-for module in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml ); do
-  echo ""
-  echo "Deleting terraform state for module "$module" on "$hostname""
-  /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/delete-terraform-state.sh -n "$hostname"
-done
-
-
-# Deploy all modules listed in deployed-modules file (using -r flag)
-####################################################################
-
-for module in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml ); do
-  echo ""
-  echo "Deploying module "$module" on "$hostname""
-  /bin/bash "$SCRIPT_DIR"/../"$module"/deploy.sh -n "$hostname" -v "$version" -r
-done
-
-
-# Deploy all projects listed in deployed-projects file (using -r and -s flags)
-##############################################################################
-
-for project in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml ); do
-  echo ""
-  echo "Deploying project "$project" on "$hostname""
-  /bin/bash "$SCRIPT_DIR"/../"$project"/deploy.sh -n "$hostname" -v "$version" -r -s
-done
-
-
-# Stop project containers
-##########################
-
-echo ""
-echo "Stopping project containers on "$hostname""
-
-for project in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml ); do
-  echo ""
-  echo "...stopping project containers for "$project""
-  /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/stop-project-containers.sh -n "$hostname"
-done
-
-
-# Stop module containers
-########################
-
-echo ""
-echo "Stopping module containers on "$hostname""
-
-for module in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml ); do
-  echo ""
-  echo "...stopping module containers for "$module""
-  /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/stop-module-containers.sh -n "$hostname"
-done
-
-
-# Restore module container persistent storage
-#############################################
-
-echo ""
-echo "Restoring module container persistent storage on "$hostname" with stamp "$stamp""
-
-for module in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml ); do
-  echo ""
-  echo "Restoring "$module" container persistent storage on "$hostname" with stamp "$stamp"" 
-  ansible-playbook -i "$SCRIPT_DIR"/configuration/inventory_"$hostname" "$SCRIPT_DIR"/backup-restore/restore-module.yml --extra-vars "id="$module" host_id="$hostname" backup_stamp="$stamp""
-done
-
-
-# Restore project container persistent storage
-##############################################
-
-echo ""
-echo "Restoring project container persistent storage on "$hostname" with stamp "$stamp""
-
-for project in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml ); do
-  echo ""
-  echo "Restoring "$project" container persistent storage on "$hostname" with stamp "$stamp""
-  ansible-playbook -i "$SCRIPT_DIR"/configuration/inventory_"$hostname" "$SCRIPT_DIR"/backup-restore/restore-project.yml --extra-vars "id="$project" host_id="$hostname" backup_stamp="$stamp""
-done
-
-
-# Start module containers
-#########################
-
-echo ""
-echo "Starting module containers on "$hostname""
-
-for module in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-modules_"$hostname".yml ); do
-  echo ""
-  echo "...starting module containers for "$module""
-  /bin/bash "$SCRIPT_DIR"/../"$module"/scripts-module/start-module-containers.sh -n "$hostname"
-done
-
-
-# Start project containers
-##########################
-
-echo ""
-echo "Starting project containers on "$hostname""
-
-for project in $( yq eval '.[]' "$SCRIPT_DIR"/backup-restore/deployed-projects_"$hostname".yml ); do
-  echo ""
-  echo "...starting project containers for "$project""
-  /bin/bash "$SCRIPT_DIR"/../"$project"/scripts-project/start-project-containers.sh -n "$hostname"
-done
-
 
 # Info - Completed
 ##################
 
 echo ""
 echo "Restore completed"
-
